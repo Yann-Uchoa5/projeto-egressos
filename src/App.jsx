a88379482317a979943f7cedf185cddb5468afba
Yan <ianuchoa5@gmail.com>
2025-08-24 12:30:03 -0300

    fetch: atualizando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import Formulario from './pages/Formulario/formulario'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login/Login";
+import TelaInicial from "./pages/TelaInicial/home";
+import Formulario from "./pages/Formulario/Formulario";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Formulario />
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<TelaInicial />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/formulario" element={<Formulario />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
